a06f79931c7b5b67ba32c7db86297496b01e538a
Ricardo Mauro <ricardo.demauro@outlook.com>
2023-04-01 19:15:33 -0400

    Add project files.

---
+++ OpenAIProxy.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Extensions.Configuration;
+using Standard.AI.OpenAI.Clients.OpenAIs;
+using Standard.AI.OpenAI.Models.Configurations;
+using Standard.AI.OpenAI.Models.Services.Foundations.ChatCompletions;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleAppOpenAI
+{
+    public class OpenAIProxy
+    {
+        readonly OpenAIClient openAIClient;
+
+        readonly List<ChatCompletionMessage> _messages;
+
+        readonly ChatCompletionMessage _systemMessage;
+
+        public OpenAIProxy([NotNull] IConfiguration configuration, string systemConfig)
+        {
+            var openAIConfigurations = new OpenAIConfigurations
+            {
+                ApiKey = configuration["OpenAI:ApiKey"],
+                OrganizationId = configuration["OpenAI:OrganizationId"]
+            };
+
+            openAIClient = new OpenAIClient(openAIConfigurations);
+
+            _messages = new List<ChatCompletionMessage>();
+
+            _systemMessage = new ChatCompletionMessage() { Content = systemConfig, Role = "system" };
+
+        }
+
+        void StackMessages(params ChatCompletionMessage[] message)
+        {
+            _messages.AddRange(message);
+        }
+
+        ChatCompletionMessage[] ToCompletitionMessage(ChatCompletionChoice[] choices)
+            => choices.Select(x => x.Message).ToArray();
+
+        public Task<ChatCompletionMessage[]> SendChatMessage(string message)
+        {
+            var chatMsg = new ChatCompletionMessage() { Content = message, Role = "user" };
+            return SendChatMessage(chatMsg);
+        }
+
+        public async Task<ChatCompletionMessage[]> SendChatMessage(ChatCompletionMessage message)
+        {
+            StackMessages(message);
+
+            var chatCompletion = new ChatCompletion
+            {
+                Request = new ChatCompletionRequest
+                {
+                    Model = "gpt-3.5-turbo",
+                    Messages = _messages.ToArray(),
+                    Temperature = 0.2,
+                    MaxTokens = 800
+                }
+            };
+
+            ChatCompletion resultChatCompletion = await openAIClient.ChatCompletions.SendChatCompletionAsync(chatCompletion);
+
+            var choices = resultChatCompletion.Response.Choices;
+
+            var messages = ToCompletitionMessage(choices);
+
+            StackMessages(messages);
+
+            return messages;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleAppOpenAI;
+using Humanizer;
+using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+using System.Reflection;
+
+AnsiConsole.MarkupLine("Starting commandline for [underline bold green]Chat GPT[/] World!");
+
+AnsiConsole.Write(new FigletText("Console GPT").Color(Color.Blue));
+
+var config = BuildConfig();
+var chatOpenAI = new OpenAIProxy(config, "You are a helpful assistant called Felix AI");
+
+var msg = AnsiConsole.Ask<string>("[bold blue]Type your first Prompt[/]:");
+do
+{
+    var results = await chatOpenAI.SendChatMessage(msg);
+
+    foreach (var item in results)
+    {
+        AnsiConsole.MarkupLineInterpolated($"[red]{item.Role.Humanize(LetterCasing.Title)}: [/] {item.Content}");
+    }
+
+    msg = AnsiConsole.Ask<string>("[bold blue]Next Prompt[/]:");
+} while (msg != "q");
+
+
+static IConfiguration BuildConfig()
+{
+    var dir = Directory.GetCurrentDirectory();
+    var configBuilder = new ConfigurationBuilder()
+        .AddJsonFile(Path.Combine(dir, "appsettings.json"), optional: false)
+        .AddUserSecrets(Assembly.GetExecutingAssembly());
+
+    return configBuilder.Build();
+}
